e69792bd305604225537c6fa830f1179f4c63418
clanga <[Your>
2023-08-02 15:46:04 +0200

    Extract and convert kelvin to celsius, and update response

--- server/wheather/router/wheather-router.js
+++ server/wheather/router/wheather-router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const HttpStatus = require("http-status-codes");
 const router = express.Router();
 const weatherService = require("../service/weather-service");
 
@@ -6,14 +7,30 @@ const weatherService = require("../service/weather-service");
  * http://localhost:5000/api/wheather/country/maputo
  */
 router.get("/country/:city", async (req, res) => {
-    try {
-        const city = req.params.city;
-        const data = await weatherService.getCityWeatherForecast(city);
-    
-        res.json(data);
-    } catch (error) {
-        res.status(500).json({ error: 'Unable to fetch weather data' });
+try {
+    const city = req.params.city;
+    const data = await weatherService.getCityWeatherForecast(city);
+
+    // Extract the temperature from the API response
+    const temperatureInKelvin = data.main.temp;
+
+    // Convert the temperature to Celsius
+    const temperatureInCelsius = temperatureInKelvin - 273.15;
+
+    // Round the temperature
+    const temperature = Math.round(temperatureInCelsius);
+
+    //send the response
+    res
+    .status(HttpStatus.StatusCodes.ACCEPTED)
+    .json({
+        message: "fetch weather data successfully",
+        temperature
+    });
+
+} catch (error) {
+    res.status(500).json({ error: "Unable to fetch weather data" });
 }
-})
+});
 
 module.exports = router;
